62d6e40c2994d4d749faf8f27fb0fc4d72bc0754
mirror <mirror12k@gmail.com>
2022-11-06 07:13:05 -0500

    - added cors headers

--- src/Program.cs
+++ src/Program.cs
@@ -3,25 +3,35 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // builder.Services
-//   .AddGraphQLServer()
-//   .AddQueryType<Query>();
+//    .AddGraphQLServer()
+//    .AddQueryType<Query>();
 
 builder.Services
   .AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.Services.AddGraphQL(b => b
-    .AddAutoSchema<Query>()  // schema
-    .AddSystemTextJson());   // serializer
+	.AddAutoSchema<Query>()  // schema
+	.AddSystemTextJson())    // serializer
+	.AddCors(options =>
+			{
+				options.AddPolicy("DefaultPolicy", builder =>
+				{
+					builder.AllowAnyHeader()
+							.WithMethods("GET", "POST")
+							.WithOrigins("*");
+				});
+			});
 
 var app = builder.Build();
 
 app.UseRouting();
 app.UseGraphQLPlayground(
-    "/",                               // url to host Playground at
-    new GraphQL.Server.Ui.Playground.PlaygroundOptions {
-        GraphQLEndPoint = "/graphql",         // url of GraphQL endpoint
-        SubscriptionsEndPoint = "/graphql",   // url of GraphQL endpoint
-    });
+	"/",
+	new GraphQL.Server.Ui.Playground.PlaygroundOptions {
+		GraphQLEndPoint = "/graphql",
+		SubscriptionsEndPoint = "/graphql",
+	});
 
+app.UseCors("DefaultPolicy");
 app.UseEndpoints(endpoints =>
   endpoints.MapGraphQL());
 
