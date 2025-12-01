11edd392b064fb27d4ac1354add90e40954561ea
Jeremy Went <jwent@users.noreply.github.com>
2025-12-01 15:58:54 -0500

    Handle malformed password hashes

--- src/Util.cs
+++ src/Util.cs
@@ -36,26 +36,42 @@ public class Util {
 		);
 	}
 
-	public static bool VerifyHash(string input, string hashString) {
-		string[] segments = hashString.Split(segmentDelimiter);
-		byte[] hash = Convert.FromHexString(segments[0]);
-		byte[] salt = Convert.FromHexString(segments[1]);
+        public static bool VerifyHash(string input, string hashString) {
+                if (string.IsNullOrWhiteSpace(hashString))
+                        return false;
 
-		// dont use dynamic iterations and hash names, this is abusable.
-		// only use this if you strictly whitelist the allowed values.
-		// // int iterations = int.Parse(segments[2]);
-		// // HashAlgorithmName algorithm = new HashAlgorithmName(segments[3]);
+                string[] segments = hashString.Split(segmentDelimiter);
+                if (segments.Length < 3)
+                        return false;
 
-		byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(
-			input,
-			salt,
-			_iterations,
-			_algorithm,
-			hash.Length
-		);
+                byte[] hash;
+                byte[] salt;
+                try {
+                        hash = Convert.FromHexString(segments[0]);
+                        salt = Convert.FromHexString(segments[1]);
+                }
+                catch (FormatException) {
+                        return false;
+                }
 
-		return CryptographicOperations.FixedTimeEquals(inputHash, hash);
-	}
+                if (hash.Length == 0 || salt.Length == 0)
+                        return false;
+
+                // dont use dynamic iterations and hash names, this is abusable.
+                // only use this if you strictly whitelist the allowed values.
+                // // int iterations = int.Parse(segments[2]);
+                // // HashAlgorithmName algorithm = new HashAlgorithmName(segments[3]);
+
+                byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(
+                        input,
+                        salt,
+                        _iterations,
+                        _algorithm,
+                        hash.Length
+                );
+
+                return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+        }
 
 	public static string GetSecureRandomString(int stringLength) {
 		return BitConverter.ToString(RandomNumberGenerator.GetBytes(stringLength / 2)).Replace("-", string.Empty);
